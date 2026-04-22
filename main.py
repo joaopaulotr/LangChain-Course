a27143daf506f041a69df678320cb2d25d281b06
João Paulo <joaopaulotraguetta14@gmail.com>
2026-04-21 22:45:55 -0300

    add: enviroment setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+# Variáveis de ambiente
+load_dotenv()
+
+def main():
+    print("Hello from Langchain course!")
+
+
+if __name__ == "__main__":
+    main()
