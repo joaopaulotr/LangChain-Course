ef7a28eac1d33b9cb4a9366ea8bdaf85704c7bf4
João Paulo <joaopaulotraguetta14@gmail.com>
2026-04-22 18:03:22 -0300

    pydantic models

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import os
-from dotenv                     import load_dotenv
-from langchain.agents           import create_agent
-from langchain.tools            import tool
-from langchain_core.messages    import HumanMessage
-from langchain_openai           import ChatOpenAI
-from langchain_tavily           import TavilySearch
+from typing import List
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    answer: str = Field(
+        description="The answer to the user's query"
+        )
+    
+    sources: List[Source] = Field(
+        default_factory=list, 
+        description="List of sources used to generate the answer"
+        )
+
+
+
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
